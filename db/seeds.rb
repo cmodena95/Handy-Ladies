fe698c430a07b2ba474af9197b1396cac892a88b
Leo Cur <leonardo.cur@gmail.com>
2020-12-09 11:02:12 +0100

    seeds created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,162 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+norma = User.create!(
+name = "Norma",
+address = "Carrer de Sant Antoni Maria Claret, 192, 08025 Barcelona",
+lat = 41.4098557,
+long = 2.1640075,
+bio = "I'm able to help you out with any carpenter job or wood repair you need, don't hesitate to contact me",
+email = "norma@gmail.com",
+password = "123456",
+available_until = )
+
+natalie = User.create!(
+name = "Natalie",
+address = "Carrer del Consell de Cent, 445, 08013 Barcelona, España",
+lat = 41.3980417,
+long = 2.1724029,
+bio = "Hey there! here I am, the best plumber in town, don't get stuck (like the sink is)  and book an appointment with me to solve it ",
+email = "natalie@gmail.com",
+password = "123456",
+available_until = )
+
+natalie = User.create!(
+name = "Katty",
+address = "Via Augusta, 268, 08017 Barcelona",
+lat = 41.3980379,
+long = 2.1293558,
+bio = "Hello girls, I'm offering my professional service of moving, medium stuffs like furnitures, plants, boxes and everything that could fit in my van.  I can go anywhere",
+email = "katty@gmail.com",
+password = "123456",
+available_until = )
+
+susan = User.create!(
+name = "Susan",
+address = "Carrer de Pelai, 5, 08001 Barcelona",
+lat = 41.385906,
+long = 2.1639056,
+bio = "Hello everyone! I have several years of experience painting houses, text me to talk about the project you have in mind =)",
+email = "susan@gmail.com",
+password = "123456",
+available_until = )
+
+olivia = User.create!(
+name = "Olivia",
+address = "Carrer de la Indústria, 241, 08041 Barcelona",
+lat = 41.4127396,
+long = 2.1761666,
+bio = "Hi, I'm Olivia, owner of a small gardening company where all the workers are women we believe in female empowerment and we work for it,
+Contact us to clear any doubt you may have about your gardering  project",
+email = "olivia@gmail.com",
+password = "123456",
+available_until = )
+
+amelia = User.create!(
+name = "Amelia",
+address = "Carrer de Mallorca, 181, 08036 Barcelona",
+lat = 41.3894736,
+long = 2.154232,
+bio = "Burned out trying to follow IKEA user manuals to assemble your new furniture? leave it to me, I can do it incredible fast. Hit me up! ",
+email = "amelia@gmail.com",
+password = "123456",
+available_until = )
+
+sophia = User.create!(
+name = "Sophia",
+address = "Carrer de Ca l'Alegre de Dalt, 9, 08024 Barcelona",
+lat = 41.4072125,
+long = 2.1606342,
+bio = "Stop searching! the electrical technician you were looking for is here! I`am Sophia and I have a long experience in electricians duties. Check my reviews, all happy customers. DM me!",
+email = "sophia@gmail.com",
+password = "123456",
+available_until = )
+
+grace = User.create!(
+name = "Grace",
+address = "Carrer de Còrsega, 417, 08037 Barcelona",
+lat = 41.4020812,
+long =2.156101,
+bio = "* Carpenter * Bricklayer * Painter Bests rates reviews in Handyladies app, don't hesitate to contact me",
+email = "grace@gmail.com",
+password = "123456",
+available_until = )
+
+valent = User.create!(
+name = "Valentina",
+address = "Carrer dels Assaonadors, 19, 08003 Barcelona",
+lat = 41.3835446,
+long = 2.1805807,
+bio = "Thinking of doing a home renovation? you are in the right profile, DM me to make it real",
+email = "valentina@gmail.com",
+password = "123456"
+available_until = )
+
+juliet = User.create!(
+name = "Juliet",
+address = "C. del Consell de Cent, 519, 08013 Barcelona",
+lat = 41.4033903,
+long = 2.1851643,
+bio = "Hi there, this is Juliet and I'm able to do housekeeping around Barcelona city center for the best price you can find. Let's try? ",
+email = "juliet@gmail.com",
+password = "123456",
+available_until = )
+
+maggie = User.create!(
+name = "Maggie",
+address = "Carrer de Roger de Flor, 262, 08025 Barcelona",
+lat = 41.4036582,
+long = 2.1696618,
+bio = "Problems with your laptop or pc? wifi doesn't work properly and you already tried everything?
+Here is the solution you need, I'm the best computer technician to help you, just let me know to set a visit ",
+email = "maggie@gmail.com",
+password = "123456",
+available_until = )
+
+
+nina = User.create!(
+name = "Nina",
+address = "Rambla de Catalunya, 64, 08007 Barcelona",
+lat = 41.388616,
+long = 2.1626232,
+bio = "restoration of wooden floors with the best technique. Let me know to create a budget",
+email = "nina@gmail.com",
+password = "123456",
+available_until = )
+
+melissa = User.create!(
+name = "Melissa",
+address = "Carrer de Còrsega, 226, Ent 2ª, 08036 Barcelona",
+lat =41.3899532,
+long = 2.1513699,
+bio = "Need help with home decoration? I can do that for you",
+email = "melissa@gmail.com",
+password = "123456",
+available_until = )
+
+
+selena = User.create!(
+name = "Selena",
+address = "Carrer de Balmes, 12, 08007 Barcelona",
+lat = 41.387806,
+long = 2.160864,
+bio = "Looking for some home renovation? You are in the best painter profile in Handy Ladies, we have hundred of happy customers  with the job done. Want to know more? text me ",
+email = "selena@gmail.com",
+password = "123456",
+available_until = )
+
+victoria = = User.create!(
+name = "Victoria",
+address = "C/ d'Aragó, 323, 325, 08009 Barcelona",
+lat = 41.3962504,
+long = 2.168011,
+bio = "Hello word! I have a plenty free time and also plenty love for dogs so I'm the right person to walk your dogs, daily, weekly or whenever you need text me to arrange a visit",
+email = "victoria@gmail.com",
+password = "123456",
+available_until = )
+
+
+
+
+
