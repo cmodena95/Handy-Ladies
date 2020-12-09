3a27f4f03741f06252bc18f5f13c0c8d79769e9e
Leo Cur <leonardo.cur@gmail.com>
2020-12-09 13:29:11 +0100

    seeds and model problem fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :reviews
-  has_many :user_skills
+  has_many :reviews, dependent: :destroy
+  has_many :user_skills, dependent: :destroy
   has_many :skills, through: :user_skills
   has_many :customer_jobs, class_name: 'Job', foreign_key: 'customer_id'
   has_many :handylady_jobs, class_name: 'Job', foreign_key: 'handylady_id'
 
   # Validations for User
-  validates :name, :address, :bio, :title, presence: true
+  validates :name, :address, :bio, presence: true
   validates :bio, length: { minimum: 10 }
 
   def messages(user)
