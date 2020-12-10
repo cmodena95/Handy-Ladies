ed7a053d0c3cdff7656178c5c166e83e041516bd
Camila Modena <itscamilla@hotmail.com>
2020-12-10 12:34:55 +0100

    Added more user skills

--- app/controllers/workers_controller.rb
+++ app/controllers/workers_controller.rb
@@ -3,8 +3,8 @@ class WorkersController < ApplicationController
   def show
     # @user = User.find(params[:id])
   end
+
   def index
     @workers = User.handyladies
-
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require "open-uri"
+
 puts "Deleting all data..."
 Job.destroy_all
 Message.destroy_all
@@ -9,7 +11,8 @@ puts "completed"
 
 puts "Creating users..."
 
-norma = User.create!(
+file = URI.open()
+norma = User.new(
   name: "Norma",
   address: "Barcelona, Spain",
   latitude: 41.4098557,
@@ -20,8 +23,11 @@ norma = User.create!(
   handylady: true,
   available_until: Time.now.advance(hour: 3)
 )
+norma.photo.attach(io: file, filename: 'user.png', content_type: 'image/png')
+norma.save!
 
-louisa = User.create!(
+file = URI.open()
+louisa = User.new(
   name: "Louisa",
   address: "Madrid, Spain",
   latitude: 40.4168,
@@ -32,8 +38,11 @@ louisa = User.create!(
   handylady: true,
   available_until: Time.now.advance(hour: 3)
 )
+louisa.photo.attach(io: file, filename: 'user.png', content_type: 'image/png')
+louisa.save!
 
-francis = User.create!(
+file = URI.open()
+francis = User.new(
   name: "Francis",
   address: "Madrid, Spain",
   latitude: 40.4168,
@@ -44,8 +53,11 @@ francis = User.create!(
   handylady: true,
   available_until: Time.now.advance(hour: 3)
 )
+francis.photo.attach(io: file, filename: 'user.png', content_type: 'image/png')
+francis.save!
 
-beth = User.create!(
+file = URI.open()
+beth = User.new(
   name: "Beth",
   address: "Sitges, Spain",
   latitude: 41.2372,
@@ -56,35 +68,56 @@ beth = User.create!(
   handylady: true,
   available_until: Time.now.advance(hour: 3)
 )
+beth.photo.attach(io: file, filename: 'user.png', content_type: 'image/png')
+beth.save!
 
-natalie = User.create!(
+file = URI.open()
+natalie = User.new(
 name: "Natalie",
 address: "Barcelona, Spain",
 latitude: 41.3980417,
 longitude: 2.1724029,
 bio:"Hey there! here I am, the best plumber in town, don't get stuck (like the sink is)  and book an appointment with me to solve it ",
 email: "natalie@gmail.com",
-password:"123456")
+password:"123456",
+handylady: true,
+available_until: Time.now.advance(hour: 3)
+)
+natalie.photo.attach(io: file, filename: 'user.png', content_type: 'image/png')
+natalie.save!
 
-katty = User.create!(
+file = URI.open()
+katty = User.new(
 name: "Katty",
 address: "Barcelona, Spain",
 latitude: 41.3980379,
 longitude: 2.1293558,
 bio: "Hello girls, I'm offering my professional service of moving, medium stuffs like furnitures, plants, boxes and everything that could fit in my van.  I can go anywhere",
 email: "katty@gmail.com",
-password:"123456")
+password:"123456",
+handylady: true,
+available_until: Time.now.advance(hour: 3)
+)
+katty.photo.attach(io: file, filename: 'user.png', content_type: 'image/png')
+katty.save!
 
-susan = User.create!(
+file = URI.open()
+susan = User.new(
 name: "Susan",
 address: "Barcelona, Spain",
 latitude: 41.385906,
 longitude: 2.1639056,
 bio: "Hello everyone! I have several years of experience painting houses, text me to talk about the project you have in mind =)",
 email: "susan@gmail.com",
-password:"123456")
+password:"123456",
+handylady: true,
+available_until: Time.now.advance(hour: 3)
+)
+susan.photo.attach(io: file, filename: 'user.png', content_type: 'image/png')
+susan.save!
 
-olivia = User.create!(
+file = URI.open()
+olivia = User.new(
 name: "Olivia",
 address: "Barcelona, Spain",
 latitude: 41.4127396,
@@ -92,54 +125,90 @@ longitude: 2.1761666,
 bio: "Hi, I'm Olivia, owner of a small gardening company where all the workers are women we believe in female empowerment and we work for it,
 Contact us to clear any doubt you may have about your gardering  project",
 email: "olivia@gmail.com",
-password:"123456")
+password:"123456",
+handylady: true,
+available_until: Time.now.advance(hour: 3)
+)
+olivia.photo.attach(io: file, filename: 'user.png', content_type: 'image/png')
+olivia.save!
 
-amelia = User.create!(
+file = URI.open()
+amelia = User.new(
 name: "Amelia",
 address: "Barcelona, Spain",
 latitude: 41.3894736,
 longitude: 2.154232,
 bio: "Burned out trying to follow IKEA user manuals to assemble your new furniture? leave it to me, I can do it incredible fast. Hit me up!",
 email: "amelia@gmail.com",
-password:"123456")
+password:"123456",
+handylady: true,
+available_until: Time.now.advance(hour: 3)
+)
+amelia.photo.attach(io: file, filename: 'user.png', content_type: 'image/png')
+amelia.save!
 
-sophia = User.create!(
+file = URI.open()
+sophia = User.new(
 name: "Sophia",
 address: "Barcelona, Spain",
 latitude: 41.4072125,
 longitude: 2.1606342,
 bio: "Stop searching! the electrical technician you were looking for is here! I`am Sophia and I have a long experience in electricians duties. Check my reviews, all happy customers. DM me!",
 email: "sophia@gmail.com",
-password:"123456")
+password:"123456",
+handylady: true,
+available_until: Time.now.advance(hour: 3)
+)
+sophia.photo.attach(io: file, filename: 'user.png', content_type: 'image/png')
+sophia.save!
 
-grace = User.create!(
+file = URI.open()
+grace = User.new(
 name: "Grace",
 address: "Barcelona, Spain",
 latitude: 41.4020812,
 longitude: 2.156101,
 bio: "* Carpenter * Bricklayer * Painter Bests rates reviews in Handyladies app, don't hesitate to contact me",
 email: "grace@gmail.com",
-password:"123456")
+password:"123456",
+handylady: true,
+available_until: Time.now.advance(hour: 3)
+)
+grace.photo.attach(io: file, filename: 'user.png', content_type: 'image/png')
+grace.save!
 
-valentina = User.create!(
+file = URI.open()
+valentina = User.new(
 name: "Valentina",
 address: "Barcelona, Spain",
 latitude: 41.3835446,
 longitude: 2.1805807,
 bio: "Thinking of doing a home renovation? you are in the right profile, DM me to make it real",
 email:  "valentina@gmail.com",
-password:"123456")
+password:"123456",
+handylady: true,
+available_until: Time.now.advance(hour: 3)
+)
+valentina.photo.attach(io: file, filename: 'user.png', content_type: 'image/png')
+valentina.save!
 
-juliet = User.create!(
+file = URI.open()
+juliet = User.new(
 name: "Juliet",
 address: "Barcelona, Spain",
 latitude: 41.4033903,
 longitude: 2.1851643,
 bio: "Hi there, this is Juliet and I'm able to do housekeeping around Barcelona city center for the best price you can find. Let's try? ",
 email: "juliet@gmail.com",
-password:"123456")
+password:"123456",
+handylady: true,
+available_until: Time.now.advance(hour: 3)
+)
+juliet.photo.attach(io: file, filename: 'user.png', content_type: 'image/png')
+juliet.save!
 
-maggie = User.create!(
+file = URI.open()
+maggie = User.new(
 name: "Maggie",
 address: "Barcelona, Spain",
 latitude: 41.4036582,
@@ -147,45 +216,69 @@ longitude: 2.1696618,
 bio: "Problems with your laptop or pc? wifi doesn't work properly and you already tried everything?
 Here is the solution you need, I'm the best computer technician to help you, just let me know to set a visit ",
 email: "maggie@gmail.com",
-password:"123456")
+password:"123456",
+handylady: true,
+available_until: Time.now.advance(hour: 3)
+)
+maggie.photo.attach(io: file, filename: 'user.png', content_type: 'image/png')
+maggie.save!
 
-
-nina = User.create!(
+file = URI.open()
+nina = User.new(
 name: "Nina",
 address: "Barcelona, Spain",
 latitude: 41.388616,
 longitude: 2.1626232,
 bio: "restoration of wooden floors with the best technique. Let me know to create a budget",
 email: "nina@gmail.com",
-password:"123456")
+password:"123456",
+handylady: true,
+available_until: Time.now.advance(hour: 3)
+)
+nina.photo.attach(io: file, filename: 'user.png', content_type: 'image/png')
+nina.save!
 
-melissa = User.create!(
+file = URI.open()
+melissa = User.new(
 name: "Melissa",
 address: "Barcelona, Spain",
 latitude: 41.3899532,
 longitude: 2.1513699,
 bio: "Need help with home decoration? I can do that for you",
 email: "melissa@gmail.com",
-password:"123456")
+password:"123456",
+handylady: true,
+available_until: Time.now.advance(hour: 3)
+)
+melissa.photo.attach(io: file, filename: 'user.png', content_type: 'image/png')
+melissa.save!
 
-
-selena = User.create!(
+file = URI.open()
+selena = User.new(
 name: "Selena",
 address: "Barcelona, Spain",
 latitude: 41.387806,
 longitude: 2.160864,
 bio:  "Looking for some home renovation? You are in the best painter profile in Handy Ladies, we have hundred of happy customers  with the job done. Want to know more? text me ",
 email: "selena@gmail.com",
-password:"123456")
+password:"123456",
+handylady: true,
+available_until: Time.now.advance(hour: 3)
+)
+selena.photo.attach(io: file, filename: 'user.png', content_type: 'image/png')
+selena.save!
 
-victoria = User.create!(
+file = URI.open()
+victoria = User.new(
 name: "Victoria",
 address: "Barcelona, Spain",
 latitude: 41.3962504,
 longitude: 2.168011,
-bio: "Hello word! I have a plenty free time and also plenty love for dogs so I'm the right person to walk your dogs, daily, weekly or whenever you need text me to arrange a visit",
+bio: "Single mum, walks primarily via the means of handstands. Enjoys sunsets and Swedish death metal.",
 email: "victoria@gmail.com",
 password:"123456")
+victoria.photo.attach(io: file, filename: 'user.png', content_type: 'image/png')
+victoria.save!
 
 puts "completed"
 
@@ -408,4 +501,94 @@ us15 = UserSkill.create!(
   price: 5,
   user: beth,
   skill: carpenter
+  )
+
+us16 = UserSkill.create!(
+  price: 30,
+  user: natalie,
+  skill: plumber
+  )
+
+us15 = UserSkill.create!(
+  price: 25,
+  user: katty,
+  skill: moving
+  )
+
+us16 = UserSkill.create!(
+  price: 40,
+  user: susan,
+  skill: painter
+  )
+
+us17 = UserSkill.create!(
+  price: 30,
+  user: olivia,
+  skill: gardening
+  )
+
+us18 = UserSkill.create!(
+  price: 5,
+  user: amelia,
+  skill: moving
+  )
+
+us19 = UserSkill.create!(
+  price: 5,
+  user: sophia,
+  skill: electrical_technician
+  )
+
+us20 = UserSkill.create!(
+  price: 50,
+  user: grace,
+  skill: carpenter
+  )
+
+us21 = UserSkill.create!(
+  price: 50,
+  user: grace,
+  skill: bricklayer
+  )
+
+us22 = UserSkill.create!(
+  price: 50,
+  user: grace,
+  skill: painter
+  )
+
+us23 = UserSkill.create!(
+  price: 50,
+  user: valentina,
+  skill: decor
+  )
+
+us24 = UserSkill.create!(
+  price: 20,
+  user: juliet,
+  skill: cleaning
+  )
+
+us25 = UserSkill.create!(
+  price: 50,
+  user: maggie,
+  skill: computer_technician
+  )
+
+us26 = UserSkill.create!(
+  price: 40,
+  user: nina,
+  skill: carpenter
+  )
+
+us27 = UserSkill.create!(
+  price: 50,
+  user: melissa,
+  skill: decor
+  )
+
+us28 = UserSkill.create!(
+  price: 20,
+  user: selena,
+  skill: decor
   )
